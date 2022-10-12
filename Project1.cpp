ebffe9a9e96d0871b454c57e6c04b602ac1e660d
def.py_02 <96482449+def-py02@users.noreply.github.com>
2022-10-12 14:05:34 +0500

    Add files via upload

---
+++ Project1.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <stdlib.h> 
+#include <time.h> 
+
+int grn(int min, int max)
+{
+	srand(time(NULL));
+	int num = min + rand() % (max - min + 1);
+	return num;
+}
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+	int rnd, n;
+	rnd = grn(1, 100);
+	std::cout << rnd;
+	while (1) {
+		std::cout << "Введите предпологаемое число: ";
+		std::cin >> n;
+
+		if ((std::max(rnd, n) - std::min(rnd, n)) >= 80) {
+			std::cout << "Очень холодно\n";
+		}
+
+		if (50 <= (std::max(rnd, n) - std::min(rnd, n)) && (std::max(rnd, n) - std::min(rnd, n)) <= 79) {
+			std::cout << "Холодно\n";
+		}
+
+		if (20 <= (std::max(rnd, n) - std::min(rnd, n)) && (std::max(rnd, n) - std::min(rnd, n)) <= 49) {
+			std::cout << "Тепло\n";
+		}
+
+		if (10 <= (std::max(rnd, n) - std::min(rnd, n)) && (std::max(rnd, n) - std::min(rnd, n)) <= 19) {
+			std::cout << "Очень тепло\n";
+		}
+
+		if (1 <= (std::max(rnd, n) - std::min(rnd, n)) && (std::max(rnd, n) - std::min(rnd, n)) <= 9) {
+			std::cout << "Жарко\n";
+		}
+
+
+		if (rnd == n) {
+			std::cout << "Победа\n";
+			break;
+		}
+
+	}
+
+}
